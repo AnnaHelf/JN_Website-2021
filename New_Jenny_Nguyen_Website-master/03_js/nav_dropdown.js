efa205388477a57ba68d54b9422febcbc06e26e7
AnnaHelf <anna.helfferich@gmx.net>
2021-05-06 03:50:46 +0200

    index mixins - xsmall not ok

--- New_Jenny_Nguyen_Website-master/03_js/nav_dropdown.js
+++ New_Jenny_Nguyen_Website-master/03_js/nav_dropdown.js
@@ -18,3 +18,19 @@ $(".nav_left-dropdown-btn").hover(
     }, 6000);
   }
 );
+
+/* xsmall */
+
+if ($(window).width() < 600) {
+  console.log("lol");
+  $(".nav_left-dropdown_items").hide();
+  $(".nav_left-dropdown-btn").click(function () {
+    console.log("lalala");
+    $(".nav_xsmall").show();
+  });
+}
+
+$(".fa-arrow-left").click(function () {
+  $(".nav_xsmall").hide();
+  /* $(".nav_xsmall").css("overflowY", "hidden"); */
+});
